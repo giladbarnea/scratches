60306ff8ee9074ef3eae066d696052f708f07911
giladbarnea <giladbrn@gmail.com>
2019-10-23 13:24:39 +0300

    idotavordjangoscratch

--- ts-generic-class/reddit6.ts
+++ ts-generic-class/reddit6.ts
@@ -2,15 +2,28 @@ interface Person {
     name: string;
 }
 
-function errorsAsExpected(person: Person){
-    let key: keyof typeof person;
-    return key === "radius";    
+function errorsAsExpected(person: Person) {
+    let key: keyof typeof person = "name";
+    
     // This condition will always return 'false'
     // since the types '"name"' and '"radius"' have no overlap.ts(2367)
 }
 
+declare function isPerson(person: Person): person is Person;
 
-function shouldErrorButDoesnt<T extends Person>(person: T){
-    let key: keyof typeof person;
-    return key === "radius";    // no error despite being equivalent to "errorsAsExpected".
+function shouldErrorButDoesnt<T extends Person>(person: T): boolean | string {
+    let key: keyof typeof person = "radius";
+    if (isPerson(person))
+        return key === "radius";    // no error despite being equivalent to "errorsAsExpected".
+    else
+        return "hi";
 }
+
+interface AnotherPerson extends Person {
+    radius: string;
+}
+
+let anotherPerson: AnotherPerson = {radius: "5", name: "yosi"};
+console.log(shouldErrorButDoesnt(anotherPerson));
+
+errorsAsExpected(anotherPerson);
