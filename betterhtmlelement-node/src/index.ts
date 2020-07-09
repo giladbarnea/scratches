a7ec83a96f3e83d7b8f56da574b1356ba7f9ad13
Gilad Barnea <giladbrn@gmail.com>
2020-07-09 13:17:29 +0300

    Thu 09 Jul 2020 13:17:28 IDT

---
+++ betterhtmlelement-node/src/index.ts
@@ -0,0 +1,5 @@
+import { isArray } from "betterhtmlelement";
+// const betterhtmlelement = require("betterhtmlelement");
+
+console.log('hello world');
+console.log(`isArray([]): `, isArray([]));
--- js-call-apply-bind/index.ts
+++ js-call-apply-bind/index.ts
@@ -1,14 +1,16 @@
 const obj = { num: 2 };
+const increase = function (extra: number, moreExtra: number): number {
+    return (this.num + 1) * extra * moreExtra;
+};
 // **  call: sets 'this' via first arg, then ...args
-const increase = function (extra, moreExtra) {return (this.num + 1) * extra * moreExtra;};
-const increased = increase.call(obj, 1, 1);
+const increased: number = increase.call(obj, 1, 1);
 console.log(increased); // (2+1) * 1 * 1 == 3
 
 // **  bind: sets 'this' explicitly, then ...args
-const bound = increase.bind(obj);
+const bound: (extra: number, moreExtra: number) => number = increase.bind(obj);
 console.dir(bound);
 console.log(bound(2, 100)); // (2+1) * 2 * 100 == 600
 
 // **  apply: like 'call', only with [...args]
-const applied = increase.apply(obj, [3, 10]);
+const applied: number = increase.apply(obj, [3, 10]);
 console.log(applied); // (2+1) * 3 * 10 == 90
